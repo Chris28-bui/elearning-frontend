4bb8eb6b0fd5f409102880f495422d36e16511bd
Tue Bui <tuebui2806@gmail.com>
2021-08-16 16:44:49 -0400

    Router, home page, detail page

--- Desktop/elearning-frontend/src/app/app.module.ts
+++ Desktop/elearning-frontend/src/app/app.module.ts
@@ -1,14 +1,30 @@
+import { Route } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+const routes: Routes = [
+  {path: 'course-details', component: CourseDetailsComponent},
+  {path: 'home', component: HomePageComponent},
+  //empty path
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  //wildcard, if does not match any
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseDetailsComponent,
+    HomePageComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule
   ],
